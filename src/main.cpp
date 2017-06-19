66552d0ab883a107d545fc089c3f962a5327e237
kevguy <kevlai22@uw.edu>
2017-06-20 01:22:39 +0800

    correction

--- src/main.cpp
+++ src/main.cpp
@@ -38,12 +38,8 @@ int main()
   // TODO: Initialize the pid variable.
   // twiddle update - error: ?? - max throttle 0.8
   // first try
-  // pid_s.Init(0.134611, 0.000270736, 3.05349);
-  // pid_t.Init(0.316731, 0.0000, 0.1226185);
-
-  // second try
-  pid_s.Init(0.134611, 0.000270736, 2.04349);
-  pid_t.Init(0.316731, 0.0000, 0.0226185);
+  pid_s.Init(0.134611, 0.000270736, 3.05349);
+  pid_t.Init(0.316731, 0.0000, 0.1226185);
 
   h.onMessage([&pid_s, &pid_t](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
